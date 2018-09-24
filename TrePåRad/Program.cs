6ba31a866332a527f2f5b9d9170512aec522363c
Terje Kolderup <terje@kolderup.net>
2018-09-24 11:32:03 +0200

    x

--- TrePåRad/Program.cs
+++ TrePåRad/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace TrePåRad
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var boardModel = new BoardModel();
+            while (true)
+            {
+                BoardView.Show(boardModel);
+                Console.Write("Skriv inn hvor du vil sette kryss (f.eks. \"a2\"): ");
+                var position = Console.ReadLine();
+                var col = position[0] - 'a';
+                var row = position[1] - '1';
+                var index = row * 3 + col;
+                boardModel.SetPlayer1(index);
+                BoardView.Show(boardModel);
+                Thread.Sleep(700);
+                var success = boardModel.SetRandomPlayer2();
+                if (!success) return;
+            }
         }
     }
 }
